ece8dd600c5d6bcdd7049a0035833022833191c9
badboyparadice <coldsalmonn@gmail.com>
2022-12-02 17:15:08 +0700

    add deploy

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,15 +1,22 @@
-import "@nomicfoundation/hardhat-toolbox";
-require("dotenv").config({ path: ".env" });
-const { GOERLI_DEPLOYER_PRIVATE_KEY, GEORLI_API_KEY } = process.env;
+import "dotenv/config";
 
-module.exports = {
+import * as dotenv from "dotenv";
+
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+
+require("solidity-coverage");
+
+// eslint-disable-next-line node/no-path-concat
+dotenv.config({ path: __dirname + "/.env" });
+
+const { GEORLI_API_KEY, GOERLI_DEPLOYER_PRIVATE_KEY } = process.env;
+const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
-  networks: {
-    goerli: {
-      url: `https://eth-goerli.g.alchemy.com/v2/${GEORLI_API_KEY}`,
-      accounts: [GOERLI_DEPLOYER_PRIVATE_KEY],
-    },
-  },
   solidity: {
     version: "0.8.17",
     settings: {
@@ -25,7 +32,34 @@ module.exports = {
     cache: "./cache",
     artifacts: "./artifacts",
   },
-  mocha: {
-    timeout: 40000,
+
+  networks: {
+    goerli: {
+      url: `https://eth-goerli.g.alchemy.com/v2/${GEORLI_API_KEY}`,
+      accounts: [GOERLI_DEPLOYER_PRIVATE_KEY!!],
+    },
+    polygon_mainnet: {
+      url: process.env.POLYGON_URL || "",
+      accounts:
+        process.env.POLYGON_PRIVATE_KEY !== undefined
+          ? [process.env.POLYGON_PRIVATE_KEY]
+          : [],
+    },
+  },
+  gasReporter: {
+    enabled: undefined,
+    currency: "THB",
+    outputFile: "gas-report.txt",
+    noColors: true,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    token: "MATIC",
+  },
+  etherscan: {
+    apiKey: {
+      polygonMumbai: process.env.MUMBAI_POLYGONSCAN_API_KEY || "",
+      rinkeby: process.env.ETHEREUM_ETHERSCAN_API_KEY || "",
+    },
   },
 };
+
+export default config;
