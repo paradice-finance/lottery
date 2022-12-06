1ba342e5522ae5f9cf0a49421bc38f0a5b6e5f8a
Parkemeow <parkemeow.paradice@gmail.com>
2022-12-06 11:34:05 +0700

    add test case for token case create new, config and buy

---
+++ test/Lottery.test.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+require('dotenv').config({ path: '.env' });
+
+describe('Lottery', function () {
+  let owner: any, A: any, B: any, C: any, D: any;
+
+  let Lottery;
+  let lottery;
+  let RandomNumberGenerator;
+
+  // beforeEach(async () => {
+  //   [owner, A, B, C] = await ethers.getSigners();
+  //   Lottery = await ethers.getContractFactory("Lottery");
+  //   lottery = await Lottery.deploy(
+  //     "0x6A832387425887A12239bF361D56C33D535F746f",
+  //     2,
+  //     1
+  //   );
+
+  //   RandomNumberGenerator = await ethers.getContractFactory(
+  //     "RandomNumberGenerator"
+  //   );
+  // });
+
+  it('RandomNumberGenerator', async function () {
+    expect(2).to.be.equal(2);
+  });
+
+  /* Creating a new lottery tests 
+  /  - Should revert when not owner
+  /  - Should revert when invalid current lotto status
+  /  - Should emit event LotteryOpen when success
+  */
+
+  /* Config new lottery 
+  /  - Should revert when not owners
+  /  - Should revert when previous lotteryStatus is not Completed
+  /  - Should revert when invalid _token address
+  /  - Should revert when invalid _sizeOfLottery 
+  /  - Should revert when invalid _ticketPrice
+  /  - Should revert when invalid ratio
+  /  - Should emit event ConfigLottery when success
+  */
+
+  /* Buy tickets 
+  /  - Should revert when call by invalid address
+  /  - Should revert when current lotteryStatus is not Open
+  /  - Should revert when buying ticket quantity > available tickets 
+  /  - Should revert when invalid _chosenNumbersForEachTicket
+  /  - Should emit NewBatchMint and Affiliate when success
+  /  - Should emit LotteryClose when fully sell tickets
+  /  - Should emit RequestNumbers when fully sell tickets and call drawWinningTicket()
+  */
+});
---
+++ test/readme.md
@@ -0,0 +1,16 @@
+# Test case convention
+
+- ref: https://medium.com/@stefanovskyi/unit-test-naming-conventions-dd9208eadbea
+
+#### Test name should express a specific requirement
+
+#### Test name could include the expected input or state and the expected result for that input or state
+
+#### Test name should be presented as a statement or fact of life that expresses workflows and outputs
+
+#### Test name could include the name of the tested method or class
+
+### Example
+
+- Should_ExpectedBehavior_When_StateUnderTest
+  - example: Should_ThrowException_When_AgeLessThan18
