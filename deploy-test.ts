15b2e5d8288e77604671c98a40193c4882232d9b
Parkemeow <parkemeow.paradice@gmail.com>
2022-12-07 14:54:00 +0700

    setup test success

--- deploy-test.ts
+++ deploy-test.ts
@@ -1,35 +1,38 @@
-import { ethers } from "hardhat";
+import { ethers } from 'hardhat';
 
 const { SUBSCRIPTION_ID } = process.env;
 async function main() {
-  const MockToken = await ethers.getContractFactory("MockToken");
+  const MockToken = await ethers.getContractFactory('MockToken');
 
   const token = await MockToken.deploy();
   try {
     await token.deployed();
-    console.log("Token address:", token.address);
+    console.log('Token address:', token.address);
   } catch (error: any) {
     console.log(`Deploy Error: ${error.message}`);
   }
 
-  const Lottery = await ethers.getContractFactory("Lottery");
+  const Lottery = await ethers.getContractFactory('Lottery');
 
   const lottery = await Lottery.deploy(
-    token.address,
-    2,
-    1,
-    process.env.TREASURY_ADDRESS!!
+    token.address, //_token
+    20, // _sizeOfLotteryNumbers
+    1, // _ticketPrice
+    process.env.TREASURY_ADDRESS!!, // _treasuryAddress
+    4, // _treasuryRatio
+    1, // _affiliateRatio
+    95 // _winnerRatio
   );
 
   try {
     await lottery.deployed();
-    console.log("lottery address:", lottery.address);
+    console.log('lottery address:', lottery.address);
   } catch (error: any) {
     console.log(`Lottery Deploy Error: ${error.message}`);
   }
 
   const RandomNumberGenerator = await ethers.getContractFactory(
-    "MockRandomNumberGenerator"
+    'MockRandomNumberGenerator'
   );
 
   const randomNumberGenerator = await RandomNumberGenerator.deploy(
@@ -40,7 +43,7 @@ async function main() {
   try {
     await randomNumberGenerator.deployed();
     console.log(
-      "RandomNumberGenerator address:",
+      'RandomNumberGenerator address:',
       randomNumberGenerator.address
     );
   } catch (error: any) {
