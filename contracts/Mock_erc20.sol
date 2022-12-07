1d0eefc28072083d4e83ff46901dc4b67c9b0794
badboyparadice <coldsalmonn@gmail.com>
2022-12-07 10:42:41 +0700

    fix for comment

--- contracts/Mock_erc20.sol
+++ contracts/Mock_erc20.sol
@@ -7,7 +7,7 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
  * @dev THIS CONTRACT IS FOR TESTING PURPOSES ONLY.
  */
 contract Mock_erc20 is ERC20 {
-    constructor(uint256 _supply) ERC20("PRD", "$PRD") {
+    constructor(uint256 _supply) ERC20("PaRaDice", "$PRD") {
         _mint(msg.sender, _supply * 10 ** 18);
     }
 
