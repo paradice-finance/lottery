73e39987d286de3fecce61795fd6183ee4596340
Parkemeow <parkemeow.paradice@gmail.com>
2022-12-07 15:44:02 +0700

    update test lottery contract

--- test/Lottery.test.ts
+++ test/Lottery.test.ts
@@ -60,49 +60,49 @@ describe('Lottery Contract', function () {
   describe('Config new lottery', function () {
     it('should revert when not owners', async function () {});
     it('should revert when previous lotteryStatus is not Completed', async function () {});
-    it('Should revert when invalid _token address', async function () {});
-    it('Should revert when invalid _sizeOfLottery', async function () {});
-    it('Should revert when invalid _ticketPrice', async function () {});
-    it('Should revert when invalid ratio', async function () {});
-    it('Should emit event ConfigLottery when success', async function () {});
+    it('should revert when invalid _token address', async function () {});
+    it('should revert when invalid _sizeOfLottery', async function () {});
+    it('should revert when invalid _ticketPrice', async function () {});
+    it('should revert when invalid ratio', async function () {});
+    it('should emit event ConfigLottery when success', async function () {});
   });
 
   describe('Buy tickets', function () {
-    it('Should revert when call by invalid address', async function () {});
-    it('Should revert when current lotteryStatus is not Open', async function () {});
-    it('Should revert when buying ticket quantity > available tickets ', async function () {});
-    it('Should revert when invalid _chosenNumbersForEachTicket', async function () {});
-    it("Should revert when buyer don't have enough token for transfer", async function () {});
-    it('Should emit event NewBatchMint and Affiliate when success', async function () {});
-    it('Should emit event LotteryClose when fully sell tickets', async function () {});
+    it('should revert when call by invalid address', async function () {});
+    it('should revert when current lotteryStatus is not Open', async function () {});
+    it('should revert when buying ticket quantity > available tickets ', async function () {});
+    it('should revert when invalid _chosenNumbersForEachTicket', async function () {});
+    it("should revert when buyer don't have enough token for transfer", async function () {});
+    it('should emit event NewBatchMint and Affiliate when success', async function () {});
+    it('should emit event LotteryClose when fully sell tickets', async function () {});
   });
 
   describe('Request winning number', function () {
-    it('Should revert when current lotteryStatus is not Closed', async function () {});
-    it('Should transfer to treasury address equal to pool - aff - winner', async function () {});
-    it('Should emit event RequestNumbers when success', async function () {});
+    it('should revert when current lotteryStatus is not Closed', async function () {});
+    it('should transfer to treasury address equal to pool - aff - winner', async function () {});
+    it('should emit event RequestNumbers when success', async function () {});
   });
 
   describe('Fullfil winning number', function () {
-    it('Should revert when current lotteryStatus is not Closed', async function () {});
-    it('Should revert when invalid _requestId', async function () {});
-    it('Should update lottery status to "Completed" when success', async function () {});
-    it('Should emit event WinningTicket when success', async function () {});
+    it('should revert when current lotteryStatus is not Closed', async function () {});
+    it('should revert when invalid _requestId', async function () {});
+    it('should update lottery status to "Completed" when success', async function () {});
+    it('should emit event WinningTicket when success', async function () {});
   });
 
   describe('Claim win reward', function () {
-    it('Should revert when send invalid lotteryId', async function () {});
-    it('Should revert when send invalid ticketId', async function () {});
-    it('Should revert when lotto status is not "completed"', async function () {});
-    it('Should revert when sender is not ticket owner', async function () {});
-    it('Should revert when winner claim twice', async function () {});
-    it("Should revert when can't transfer token to winner", async function () {});
-    it('Should emit event ClaimWinReward when success', async function () {});
+    it('should revert when send invalid lotteryId', async function () {});
+    it('should revert when send invalid ticketId', async function () {});
+    it('should revert when lotto status is not "completed"', async function () {});
+    it('should revert when sender is not ticket owner', async function () {});
+    it('should revert when winner claim twice', async function () {});
+    it("should revert when can't transfer token to winner", async function () {});
+    it('should emit event ClaimWinReward when success', async function () {});
   });
 
   describe('Claim affiliate', function () {
     it('should revert when lotteryStatus is not Completed', async function () {});
     it('should not transfer when total claim is zero', async function () {});
-    it('Should emit event ClaimedAffiliate when success', async function () {});
+    it('should emit event ClaimedAffiliate when success', async function () {});
   });
 });
