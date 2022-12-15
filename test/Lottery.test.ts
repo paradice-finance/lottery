0deced0aa6174d1cf7e01453fb55d7085b0ad1ab
Parkemeow <parkemeow.paradice@gmail.com>
2022-12-15 12:07:11 +0700

    finish test claimAffiliate

--- test/Lottery.test.ts
+++ test/Lottery.test.ts
@@ -6,8 +6,8 @@ import { ethers } from 'hardhat';
 require('dotenv').config({ path: '.env' });
 const { lotto } = require('./settings.ts');
 
-describe('Lottery Contract', function () {
-  let owner: any, buyer: any, buyerWithAllowance: any, C: any, D: any;
+describe('Lottery Contract', () => {
+  let owner: any, buyer: any, buyerWithAllowance: any, seller: any, D: any;
   let Token;
   let token: any;
   let Lottery;
@@ -17,14 +17,13 @@ describe('Lottery Contract', function () {
   let MockVRF: any;
   let mockVRF: any;
   let nullAddress = '0x0000000000000000000000000000000000000000';
-  let affiliateAddress = '0x1ecB3e701417D9e672300AD9b9a1747bC6E6FB79';
   let setup = lotto.setup;
   let errors = lotto.errors;
   let events = lotto.events;
   let status = lotto.status;
 
   beforeEach(async () => {
-    [owner, buyer, buyerWithAllowance, C] = await ethers.getSigners();
+    [owner, buyer, buyerWithAllowance, seller] = await ethers.getSigners();
 
     Token = await ethers.getContractFactory('Mock_erc20');
     token = await (await Token.deploy(100000)).deployed();
@@ -79,26 +78,26 @@ describe('Lottery Contract', function () {
     addConsumer = await addConsumer.wait();
   });
 
-  describe('Mock token contract', function () {
-    it('should get balance properly.', async function () {
+  describe('Mock token contract', () => {
+    it('should get balance properly.', async () => {
       expect(await token.balanceOf(owner.address)).not.be.equal(0);
       expect(await token.balanceOf(buyer.address)).to.be.equal(0);
     });
   });
 
-  describe('Creating a new lottery', function () {
-    it('should revert when not owner', async function () {
+  describe('Creating a new lottery', () => {
+    it('should revert when not owner', async () => {
       await expect(lottery.connect(buyer).createNewLotto()).to.be.revertedWith(
         errors.invalid_admin
       );
     });
-    it('should revert when invalid current lotto status', async function () {
+    it('should revert when invalid current lotto status', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(lottery.connect(owner).createNewLotto()).to.be.revertedWith(
         errors.create_new_lottery_when_previous_lottery_not_finished
       );
     });
-    it('should emit event LotteryOpen when success', async function () {
+    it('should emit event LotteryOpen when success', async () => {
       await expect(lottery.connect(owner).createNewLotto()).to.emit(
         lottery,
         events.new
@@ -106,8 +105,8 @@ describe('Lottery Contract', function () {
     });
   });
 
-  describe('Config new lottery', function () {
-    it('should revert when not owners', async function () {
+  describe('Config new lottery', () => {
+    it('should revert when not owners', async () => {
       await expect(
         lottery
           .connect(buyer)
@@ -121,7 +120,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_admin);
     });
-    it('should revert when previous lotteryStatus is not Completed', async function () {
+    it('should revert when previous lotteryStatus is not Completed', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -138,7 +137,7 @@ describe('Lottery Contract', function () {
         errors.config_new_lottery_when_previous_lottery_not_finished
       );
     });
-    it('should revert when invalid _token address', async function () {
+    it('should revert when invalid _token address', async () => {
       await expect(
         lottery
           .connect(owner)
@@ -152,7 +151,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_token_address);
     });
-    it('should revert when invalid _sizeOfLottery', async function () {
+    it('should revert when invalid _sizeOfLottery', async () => {
       await expect(
         lottery
           .connect(owner)
@@ -166,7 +165,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_lottery_size);
     });
-    it('should revert when invalid _ticketPrice', async function () {
+    it('should revert when invalid _ticketPrice', async () => {
       await expect(
         lottery
           .connect(owner)
@@ -180,7 +179,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_ticket_price);
     });
-    it('should revert when invalid ratio', async function () {
+    it('should revert when invalid ratio', async () => {
       await expect(
         lottery
           .connect(owner)
@@ -194,7 +193,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_ratio);
     });
-    it('should emit event ConfigLottery when success', async function () {
+    it('should emit event ConfigLottery when success', async () => {
       await expect(
         lottery
           .connect(owner)
@@ -210,13 +209,13 @@ describe('Lottery Contract', function () {
     });
   });
 
-  describe('Batch buy tickets', function () {
-    it('should revert when current lotteryStatus is not Open', async function () {
+  describe('Batch buy tickets', () => {
+    it('should revert when current lotteryStatus is not Open', async () => {
       await expect(
         lottery.connect(buyer).batchBuyLottoTicket(1, [1], nullAddress, false)
       ).to.be.revertedWith(errors.invalid_buy_not_open);
     });
-    it('should revert when buying ticket quantity > available tickets ', async function () {
+    it('should revert when buying ticket quantity > available tickets ', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -229,7 +228,7 @@ describe('Lottery Contract', function () {
           )
       ).to.be.revertedWith(errors.invalid_buy_to_large);
     });
-    it('should revert when invalid _chosenNumbersForEachTicket', async function () {
+    it('should revert when invalid _chosenNumbersForEachTicket', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -237,13 +236,13 @@ describe('Lottery Contract', function () {
           .batchBuyLottoTicket(3, [1, 2], nullAddress, false)
       ).to.be.revertedWith(errors.invalid_buy_chosen_number);
     });
-    it("should revert when buyer don't have enough token for transfer", async function () {
+    it("should revert when buyer don't have enough token for transfer", async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery.connect(buyer).batchBuyLottoTicket(1, [1], nullAddress, false)
       ).to.be.revertedWith(errors.invalid_buy_approve);
     });
-    it('should emit event NewBatchMint when success', async function () {
+    it('should emit event NewBatchMint when success', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -251,15 +250,15 @@ describe('Lottery Contract', function () {
           .batchBuyLottoTicket(1, [1], nullAddress, false)
       ).to.emit(lottery, events.batchBuy);
     });
-    it('should emit event Affiliate when success batch buy with affiliate address', async function () {
+    it('should emit event Affiliate when success batch buy with affiliate address', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
           .connect(buyerWithAllowance)
-          .batchBuyLottoTicket(1, [1], affiliateAddress, true)
+          .batchBuyLottoTicket(1, [1], seller.address, true)
       ).to.emit(lottery, events.affiliate);
     });
-    it('should emit event LotteryClose when fully sell tickets', async function () {
+    it('should emit event LotteryClose when fully sell tickets', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -279,8 +278,8 @@ describe('Lottery Contract', function () {
     });
   });
 
-  describe('Request winning number', function () {
-    it('should emit event RequestNumbers when success', async function () {
+  describe('Request winning number', () => {
+    it('should emit event RequestNumbers when success', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery
@@ -295,13 +294,13 @@ describe('Lottery Contract', function () {
     });
   });
 
-  describe('Fullfil winning number', function () {
-    it('should revert when caller is not RandomNumberGenerator', async function () {
+  describe('Fullfil winning number', () => {
+    it('should revert when caller is not RandomNumberGenerator', async () => {
       await expect(
         lottery.connect(owner).fullfilWinningNumber(1, 1)
       ).to.be.revertedWith(errors.invalid_random_generator);
     });
-    it('should emit event winning number and change lottery status to completed when success', async function () {
+    it('should emit event winning number and change lottery status to completed when success', async () => {
       await lottery.connect(owner).createNewLotto();
       let allEvent = await (
         await lottery
@@ -331,7 +330,7 @@ describe('Lottery Contract', function () {
     });
   });
 
-  describe('Claim win reward', function () {
+  describe('Claim win reward', () => {
     beforeEach(async () => {
       await lottery.connect(owner).createNewLotto();
       await lottery
@@ -346,34 +345,34 @@ describe('Lottery Contract', function () {
         .connect(owner)
         .fulfillRandomWords(1, randomNumberGenerator.address);
     });
-    it('should revert when send invalid lotteryId', async function () {
+    it('should revert when send invalid lotteryId', async () => {
       await expect(
         lottery.connect(buyerWithAllowance).claimWinReward(0, 0)
       ).to.be.revertedWith(errors.invalid_lottery_id);
     });
-    it('should revert when send invalid ticketId', async function () {
+    it('should revert when send invalid ticketId', async () => {
       await expect(
         lottery.connect(buyerWithAllowance).claimWinReward(1, 0)
       ).to.be.revertedWith(errors.invalid_ticket_id);
     });
-    it('should revert when lotto status is not "completed"', async function () {
+    it('should revert when lotto status is not "completed"', async () => {
       await lottery.connect(owner).createNewLotto();
       await expect(
         lottery.connect(buyerWithAllowance).claimWinReward(2, 1)
       ).to.be.revertedWith(errors.invalid_claim_not_complete);
     });
-    it('should revert when sender is not ticket owner', async function () {
+    it('should revert when sender is not ticket owner', async () => {
       await expect(
         lottery.connect(buyer).claimWinReward(1, 1)
       ).to.be.revertedWith(errors.invalid_ticket_owner);
     });
-    it('should revert when winner claim twice', async function () {
+    it('should revert when winner claim twice', async () => {
       await lottery.connect(buyerWithAllowance).claimWinReward(1, 2);
       await expect(
         lottery.connect(buyerWithAllowance).claimWinReward(1, 2)
       ).to.be.revertedWith(errors.invalid_claim_twice);
     });
-    it('should emit event ClaimWinReward when success', async function () {
+    it('should emit event ClaimWinReward when success', async () => {
       await expect(
         await lottery.connect(buyerWithAllowance).claimWinReward(1, 2)
       ).to.emit(lottery, events.claimWinReward);
@@ -384,9 +383,84 @@ describe('Lottery Contract', function () {
     });
   });
 
-  // describe('Claim affiliate', function () {
-  //   it('should revert when lotteryStatus is not Completed', async function () {});
-  //   it('should not transfer when total claim is zero', async function () {});
-  //   it('should emit event ClaimedAffiliate when success', async function () {});
-  // });
+  describe('Get affiliate ticket quantity', async () => {
+    it('should return correct ticket quantity when success', async () => {
+      await lottery.connect(owner).createNewLotto();
+      await lottery
+        .connect(buyerWithAllowance)
+        .batchBuyLottoTicket(
+          setup.sizeOfLotteryNumbers,
+          setup.chosenNumbersForEachTicket,
+          seller.address,
+          true
+        );
+      await mockVRF
+        .connect(owner)
+        .fulfillRandomWords(1, randomNumberGenerator.address);
+      const [_, ticketCount] = await lottery
+        .connect(seller)
+        .getAffiliateTicketQty([1]);
+
+      assert.equal(ticketCount, setup.sizeOfLotteryNumbers);
+    });
+  });
+
+  describe('Claim affiliate', () => {
+    beforeEach(async () => {
+      await lottery.connect(owner).createNewLotto();
+      await lottery
+        .connect(buyerWithAllowance)
+        .batchBuyLottoTicket(
+          setup.sizeOfLotteryNumbers,
+          setup.chosenNumbersForEachTicket,
+          seller.address,
+          true
+        );
+      await mockVRF
+        .connect(owner)
+        .fulfillRandomWords(1, randomNumberGenerator.address);
+    });
+    it('should revert when lotteryStatus is not Completed', async () => {
+      await lottery.connect(owner).createNewLotto();
+      await expect(
+        lottery.connect(buyerWithAllowance).claimAffiliate([2])
+      ).to.be.revertedWith(errors.invalid_claim_aff_not_complete);
+    });
+    it('should not transfer when total claim is zero', async () => {
+      // first claim
+      await lottery.connect(seller).claimAffiliate([1]);
+      let lotteryBalanceAfterFirstClaim = await token.balanceOf(
+        lottery.address
+      );
+      // second claim
+      await lottery.connect(seller).claimAffiliate([1]);
+      let lotteryBalanceAfterSecondClaim = await token.balanceOf(
+        lottery.address
+      );
+      assert.equal(
+        lotteryBalanceAfterFirstClaim.value,
+        lotteryBalanceAfterSecondClaim.value
+      );
+    });
+    it('should emit event ClaimedAffiliate when success', async () => {
+      await expect(await lottery.connect(seller).claimAffiliate([1])).to.emit(
+        lottery,
+        events.claimedAffiliate
+      );
+      let balanceAfter = await token.balanceOf(seller.address);
+      assert.equal(balanceAfter, setup.sellerBalanceAfterClaim);
+    });
+    it('should reset ticket count to zero when success', async () => {
+      await expect(await lottery.connect(seller).claimAffiliate([1])).to.emit(
+        lottery,
+        events.claimedAffiliate
+      );
+
+      const [_, ticketCount] = await lottery
+        .connect(seller)
+        .getAffiliateTicketQty([1]);
+
+      assert.equal(ticketCount, 0);
+    });
+  });
 });
