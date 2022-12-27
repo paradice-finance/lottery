cb019af357caeff8531c44315372bd47d5bde49e
Parkemeow <parkemeow.paradice@gmail.com>
2022-12-27 14:39:44 +0700

    update deploy script on test net

--- deploy-test.ts
+++ deploy-test.ts
@@ -18,6 +18,7 @@ async function main() {
   const lottery = await Lottery.deploy(
     token.address, //_token
     5, // _sizeOfLotteryNumbers
+    999999,
     1000000000000000000n, // _ticketPrice
     process.env.TREASURY_ADDRESS!!, // _treasuryAddress
     4, // _treasuryRatio
